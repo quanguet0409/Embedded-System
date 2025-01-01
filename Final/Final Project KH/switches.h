a4b76b52d45714a2f3155c182deafd85a70eb36a
ninhhaidang <ninhhaidangg@gmail.com>
2025-01-01 23:20:31 +0700

    Final Project
    Lastest Update

--- Final/Final Project KH/switches.h
+++
@@ -1,9 +0,0 @@
-#ifndef SWITCHES_H
-#define SWITCHES_H
-
-#include <stdint.h>
-
-void init_switches(void);
-void PORTC_PORTD_IRQHandler(void);
-
-#endif
--- Final/Final Project KH/systick.h
+++
@@ -1,7 +0,0 @@
-#ifndef SYSTICK_H
-#define SYSTICK_H
-
-void init_systick(void);
-void delay_ms(uint32_t ms);
-
-#endif
